f7034017d18254ca911b96d2f2d1f776f38d7fcc
ricardo <ricardorbxx1@gmail.com>
2023-09-05 09:25:09 -0300

    Changed structure of tests files

--- templates/extra/rtl/src/tests/App.test.tsx
+++ templates/extra/rtl/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import App from '../App';
 
 it('should show "Vite + React + Trybe" text', () => {
   render(<App />);
--- templates/extra/rtlrouter/src/tests/App.test.tsx
+++ templates/extra/rtlrouter/src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from '../App';
 
 it('should show "Vite + React" text', () => {
   render(
