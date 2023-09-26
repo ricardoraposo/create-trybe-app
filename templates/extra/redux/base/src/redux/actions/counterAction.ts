5510e2d8b36057d5c78b6378eabb20c49c9b430b
ricardo <ricardorbxx1@gmail.com>
2023-09-26 14:57:49 -0300

    erro de dientacao

--- templates/extra/redux/base/src/redux/actions/counterAction.ts
+++ templates/extra/redux/base/src/redux/actions/counterAction.ts
@@ -1,6 +1,6 @@
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 
 export const counterAction = (payload = 1) => ({
-    type: INCREMENT_COUNTER,
-    payload,
+  type: INCREMENT_COUNTER,
+  payload,
 });
